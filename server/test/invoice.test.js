db2a9b9cba354388eef56630397c3c12e3d42760
Ciaran M <cmorinan@gmail.com>
2018-12-18 14:34:20 +0000

    added tests for invoice model and server

---
+++ server/test/invoice.test.js
@@ -0,0 +1,52 @@
+const expect = require('chai').expect
+
+const {Invoice} = require('../db/invoice')
+
+describe('Invoice model', () => {
+  let invoice = new Invoice({ debtor: "ABC Cars", due: "2018-12-12", value: 500, service: "Maintenance" })
+  let emptyInvoice = new Invoice()
+
+  it('should save an invoice created with valid data', (done) => {
+    invoice.validate((err) => {
+      expect(err).to.not.exist
+      done()
+    })
+  })
+
+  it('should be invalid if the debtor company is not supplied', (done) => {
+    emptyInvoice.validate((err) => {
+      expect(err.errors.debtor).to.exist
+      done()
+    })
+  })
+
+  it('should be invalid if the value is not supplied', (done) => {
+    emptyInvoice.validate((err) => {
+      expect(err.errors.value).to.exist
+      done()
+    })
+  })
+
+  it('should be invalid if the due date is not supplied', (done) => {
+    emptyInvoice.validate((err) => {
+      expect(err.errors.due).to.exist
+      done()
+    })
+  })
+
+  it('should be invalid if the service is not supplied', (done) => {
+    emptyInvoice.validate((err) => {
+      expect(err.errors.service).to.exist
+      done()
+    })
+  })
+
+  it ('should be invalid if paid is set to true without providing a date', (done) => {
+    invoice.paidStatus = true
+    invoice.validate((err) => {
+      expect(err.errors.paidDate).to.exist
+      done()
+    })
+  })
+
+})
